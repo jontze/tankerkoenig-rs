5d25fb32dbfbbe40daf034e9c9e78ea14ca7e77d
Jontze <42588836+jontze@users.noreply.github.com>
2022-06-06 14:50:49 +0200

    docs(models): Add documentation to models module

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,20 +1,36 @@
+//! Module contains Structs that describe all possible
+//! responses of the [Prices](crate::api::price::PriceApi)
+//! and [Station](crate::api::station::StationApi) api.
+
 pub mod price;
 pub mod station;
 
+pub use price::PriceResponse;
+pub use station::{AreaFuelResponse, AreaNearResponse, DetailsResponse};
+
+/// Enum of all supported fuel types
 #[derive(EnumString, Display, PartialEq, Debug)]
 pub enum Fuel {
+    /// Super95 or E5 and serialized to `e5`
     #[strum(serialize = "e5")]
     E5,
+    /// Super90 or E10 and serialized to `e10`
     #[strum(serialize = "e10")]
     E10,
+    /// Diesel and serialized to `diesel`
     #[strum(serialize = "diesel")]
     Diesel,
 }
 
+/// Enum of supported sorting logic
 #[derive(EnumString, Display, PartialEq, Debug)]
 pub enum Sort {
+    /// Sort by price from lowest to highest price and
+    /// will serialize to `price`
     #[strum(serialize = "price")]
     Price,
+    /// Sort by distance from near to far and
+    /// will serialize to `dist`
     #[strum(serialize = "dist")]
     Distance,
 }
