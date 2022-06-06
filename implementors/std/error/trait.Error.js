46f8a61cfc99fcb39a1b43e5228b4f1604ff295a
jontze <dev@jontze.com>
2022-06-06 13:46:17 +0000

    Deploying to gh-pages from @ jontze/tankerkoenig-rs@0e82f9d9bb0a4322e829eaf61663dd50272d4f7f 🚀

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["tankerkoenig"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"tankerkoenig/enum.Error.html\" title=\"enum tankerkoenig::Error\">TankerkoenigError</a>","synthetic":false,"types":["tankerkoenig::error::TankerkoenigError"]}];
+implementors["tankerkoenig"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"tankerkoenig/enum.Error.html\" title=\"enum tankerkoenig::Error\">TankerkoenigError</a>","synthetic":false,"types":["tankerkoenig::error::TankerkoenigError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
