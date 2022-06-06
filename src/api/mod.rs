c31bf3c9f806306804cd12ead7c6452aff799ad1
Jontze <42588836+jontze@users.noreply.github.com>
2022-06-06 14:55:13 +0200

    feat(api): Create new function with explicit user agent

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,71 +1,106 @@
-mod price;
-mod station;
+//! Module that contains the main [Tankerkoenig] struct that gives access to
+//! the two child structs [PriceApi](price::PriceApi) and [StationApi](station::StationApi).
 
-use super::utils;
+pub mod price;
+pub mod station;
+
+use crate::utils;
 use price::PriceApi;
 use station::StationApi;
 
-/// Settings struct that contains the api key the current package version and the package name.
+/// Settings struct that contains the api key, the current package version and the package name.
 /// The API key will be used as a query parameter in the requests and the package name and version
 /// will be used by default as useragent if no other useragent is provided.
 #[derive(Debug, Clone)]
 pub struct Settings {
     api_key: String,
+    /// Crate version
     pub package_version: &'static str,
+    /// Crate name
     pub package_name: &'static str,
 }
 
 /// The main struct of the crate giving access to the station and price api of tankerkoenig.
-/// Create a new instance of the struct with your api key as parameter and optional your custom
-/// useragent. If you want to submit requests with a custom useragent you can pass this as
-/// second parameter.
+/// Create a new instance of the struct with your api key as parameter.
 ///
 /// ## Example
 /// ```
 /// use tankerkoenig::Tankerkoenig;
 /// use tankerkoenig::models;
 ///
-/// async fn request_station_Details() -> Result<models::station::DetailsResponse, tankerkoenig::Error> {
-///   let tanker = Tankerkoenig::new("your-api-key", None);
-///   let details = tanker.station.fetch_detail("id-of-the-fuel-station").await?;
+/// async fn request_station_details() -> Result<models::station::DetailsResponse, tankerkoenig::Error> {
+///   let tanker = Tankerkoenig::new("your-api-key");
+///   let details = tanker.station.fetch_details("id-of-the-fuel-station").await?;
 ///   Ok(details)
 /// }
 /// ```
 #[derive(Debug, Clone)]
 pub struct Tankerkoenig {
+    /// Provide access to all station related api resources
     pub station: StationApi,
+    /// Provide access to all price related api resources
     pub price: PriceApi,
 }
 
 impl<'a> Tankerkoenig {
-    /// Creates a new instance of the Tankerkoenig struct by passing your api key as first
-    /// function parameter. Optional you can provide a custom useragent for the requests,
-    /// otherwise just pass `None` as second parameter.
+    /// Creates a new instance of the Tankerkoenig struct by passing your api key as
+    /// function parameter.
     ///
     /// ## Example
     /// ```
     /// use tankerkoenig::Tankerkoenig;
     /// use tankerkoenig::models;
     ///
-    /// async fn request_station_Details() -> Result<models::station::DetailsResponse, tankerkoenig::Error> {
-    ///   let tanker = Tankerkoenig::new("your-api-key", None);
-    ///   let details = tanker.station.fetch_detail("id-of-the-fuel-station").await?;
+    /// async fn request_station_details() -> Result<models::station::DetailsResponse, tankerkoenig::Error> {
+    ///   let tanker = Tankerkoenig::new("your-api-key");
+    ///   let details = tanker.station.fetch_details("id-of-the-fuel-station").await?;
     ///   Ok(details)
     /// }
     /// ```
-    pub fn new(api_key: &'a str, user_agent: Option<&str>) -> Self {
+    pub fn new(api_key: &'a str) -> Self {
         let settings = std::sync::Arc::new(Settings {
             package_version: env!("CARGO_PKG_VERSION"),
             package_name: env!("CARGO_PKG_NAME"),
             api_key: String::from(api_key),
         });
         let client = std::sync::Arc::new(
-            utils::baseline::construct_client(user_agent, &settings)
-                .expect("Client to be constructed"),
+            utils::baseline::construct_client(None, &settings).expect("Client to be constructed"),
         );
         Self {
             station: StationApi::new(client.clone(), settings.clone()),
             price: PriceApi::new(client, settings),
+        }
+    }
+
+    /// Creates a new instance of the Tankerkoenig struct by passing your api key as first
+    /// function parameter and your customer user agent as second parameter. Default user agent
+    /// is the current crate name the version number.
+    ///
+    /// ## Example
+    /// ```
+    /// use tankerkoenig::Tankerkoenig;
+    /// use tankerkoenig::models;
+    ///
+    /// async fn request_station_details() -> Result<models::station::DetailsResponse, tankerkoenig::Error> {
+    ///   let user_agent = "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion";
+    ///   let tanker = Tankerkoenig::new_with_useragent("your-api-key", user_agent);
+    ///   let details = tanker.station.fetch_details("id-of-the-fuel-station").await?;
+    ///   Ok(details)
+    /// }
+    /// ```
+    pub fn new_with_useragent(api_key: &'a str, user_agent: &'a str) -> Self {
+        let settings = std::sync::Arc::new(Settings {
+            package_version: env!("CARGO_PKG_VERSION"),
+            package_name: env!("CARGO_PKG_NAME"),
+            api_key: String::from(api_key),
+        });
+        let client = std::sync::Arc::new(
+            utils::baseline::construct_client(Some(user_agent), &settings)
+                .expect("Client to be constructed"),
+        );
+        Self {
+            station: StationApi::new(client.clone(), settings.clone()),
+            price: PriceApi::new(client.clone(), settings.clone()),
         }
     }
 }
