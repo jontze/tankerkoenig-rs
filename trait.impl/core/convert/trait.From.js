16ff9d7e988240473ef0f0c94892ff875c6e7954
jontze <dev@jontze.com>
2024-11-02 12:10:47 +0000

    Deploying to gh-pages from @ jontze/tankerkoenig-rs@d3a65e0243345d34887e28c522bc6915b470cee6 🚀

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tankerkoenig":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;InvalidHeaderValue&gt; for <a class=\"enum\" href=\"tankerkoenig/enum.Error.html\" title=\"enum tankerkoenig::Error\">TankerkoenigError</a>"]]
+"tankerkoenig":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;InvalidHeaderValue&gt; for <a class=\"enum\" href=\"tankerkoenig/enum.Error.html\" title=\"enum tankerkoenig::Error\">TankerkoenigError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
